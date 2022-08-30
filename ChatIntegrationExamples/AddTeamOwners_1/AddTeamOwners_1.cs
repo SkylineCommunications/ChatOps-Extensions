f26e49ba21a4cfde0d735d7a966966faea30b020
Sven Dedeene <sven.dedeene@skyline.be>
2022-08-30 10:49:41 +0200

    Update scripts to use the latest nuget.

--- ChatIntegrationExamples/AddTeamOwners_1/AddTeamOwners_1.cs
+++ ChatIntegrationExamples/AddTeamOwners_1/AddTeamOwners_1.cs
@@ -1,73 +1,14 @@
-/*
-****************************************************************************
-*  Copyright (c) 2022,  Skyline Communications NV  All Rights Reserved.    *
-****************************************************************************
-
-By using this script, you expressly agree with the usage terms and
-conditions set out below.
-This script and all related materials are protected by copyrights and
-other intellectual property rights that exclusively belong
-to Skyline Communications.
-
-A user license granted for this script is strictly for personal use only.
-This script may not be used in any way by anyone without the prior
-written consent of Skyline Communications. Any sublicensing of this
-script is forbidden.
-
-Any modifications to this script by the user are only allowed for
-personal use and within the intended purpose of the script,
-and will remain the sole responsibility of the user.
-Skyline Communications will not be responsible for any damages or
-malfunctions whatsoever of the script resulting from a modification
-or adaptation by the user.
-
-The content of this script is confidential information.
-The user hereby agrees to keep this confidential information strictly
-secret and confidential and not to disclose or reveal it, in whole
-or in part, directly or indirectly to any person, entity, organization
-or administration without the prior written consent of
-Skyline Communications.
-
-Any inquiries can be addressed to:
-
-	Skyline Communications NV
-	Ambachtenstraat 33
-	B-8870 Izegem
-	Belgium
-	Tel.	: +32 51 31 35 69
-	Fax.	: +32 51 31 01 29
-	E-mail	: info@skyline.be
-	Web		: www.skyline.be
-	Contact	: Ben Vandenberghe
-
-****************************************************************************
-Revision History:
-
-DATE		VERSION		AUTHOR			COMMENTS
-
-04/08/2022	1.0.0.1		Jordy Ampe, Skyline	Initial version
-****************************************************************************
-*/
-
 using System;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using DcpChatIntegrationHelper;
 using Skyline.DataMiner.Automation;
+using Skyline.DataMiner.DcpChatIntegrationHelper.Common;
+using Skyline.DataMiner.DcpChatIntegrationHelper.Teams;
 
-/// <summary>
-/// DataMiner Script Class.
-/// </summary>
 public class Script
 {
-	/// <summary>
-	/// The Script entry point.
-	/// </summary>
-	/// <param name="engine">Link with SLAutomation process.</param>
 	public void Run(Engine engine)
 	{
-		ChatIntegrationHelper chatIntegrationHelper = null;
+		var chatIntegrationHelper = new ChatIntegrationHelperBuilder().Build();
 		try
 		{
 			var teamIdParam = engine.GetScriptParam("Team Id");
@@ -99,17 +40,14 @@ public class Script
 				return;
 			}
 
-			var factory = new ChatIntegrationHelperFactory();
-			chatIntegrationHelper = factory.Create(
-				log => engine.Log(log, LogType.Debug, 1),
-				log => engine.Log(log, LogType.Information, 1),
-				log => engine.Log(log, LogType.Error, 1));
-
-			var response = chatIntegrationHelper.Teams.TryAddTeamOwners(teamIdParam.Value, teamOwnersToAdd);
-			if (response.Error)
+			try
+			{
+				chatIntegrationHelper.Teams.TryAddTeamOwners(teamIdParam.Value, teamOwnersToAdd);
+			}
+			catch (TeamsChatIntegrationException e)
 			{
 				engine.ExitFail(
-					$"Couldn't add the owners [{string.Join(", ", teamOwnersToAdd)}] to the team with id {teamIdParam.Value} with error {response.ErrorMessage}.");
+					$"Couldn't add the owners [{string.Join(", ", teamOwnersToAdd)}] to the team with id {teamIdParam.Value} with error {e.Message}.");
 				return;
 			}
 
