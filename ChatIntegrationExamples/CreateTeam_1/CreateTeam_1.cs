7acd4ab61e65b9cb3d42526e459b5648d9690742
Sven Dedeene <99406588+Sven-at-Skyline@users.noreply.github.com>
2022-08-30 11:38:45 +0200

    Update scripts to use the latest nuget. (#4)

--- ChatIntegrationExamples/CreateTeam_1/CreateTeam_1.cs
+++ ChatIntegrationExamples/CreateTeam_1/CreateTeam_1.cs
@@ -1,72 +1,13 @@
-/*
-****************************************************************************
-*  Copyright (c) 2022,  Skyline Communications NV  All Rights Reserved.    *
-****************************************************************************
-
-By using this script, you expressly agree with the usage terms and
-conditions set out below.
-This script and all related materials are protected by copyrights and
-other intellectual property rights that exclusively belong
-to Skyline Communications.
-
-A user license granted for this script is strictly for personal use only.
-This script may not be used in any way by anyone without the prior
-written consent of Skyline Communications. Any sublicensing of this
-script is forbidden.
-
-Any modifications to this script by the user are only allowed for
-personal use and within the intended purpose of the script,
-and will remain the sole responsibility of the user.
-Skyline Communications will not be responsible for any damages or
-malfunctions whatsoever of the script resulting from a modification
-or adaptation by the user.
-
-The content of this script is confidential information.
-The user hereby agrees to keep this confidential information strictly
-secret and confidential and not to disclose or reveal it, in whole
-or in part, directly or indirectly to any person, entity, organization
-or administration without the prior written consent of
-Skyline Communications.
-
-Any inquiries can be addressed to:
-
-	Skyline Communications NV
-	Ambachtenstraat 33
-	B-8870 Izegem
-	Belgium
-	Tel.	: +32 51 31 35 69
-	Fax.	: +32 51 31 01 29
-	E-mail	: info@skyline.be
-	Web		: www.skyline.be
-	Contact	: Ben Vandenberghe
-
-****************************************************************************
-Revision History:
-
-DATE		VERSION		AUTHOR			COMMENTS
-
-02/08/2022	1.0.0.1		Jordy Ampe, Skyline	Initial version
-****************************************************************************
-*/
-
 using System;
-using System.Threading;
-using System.Threading.Tasks;
-using DcpChatIntegrationHelper;
 using Skyline.DataMiner.Automation;
+using Skyline.DataMiner.DcpChatIntegrationHelper.Common;
+using Skyline.DataMiner.DcpChatIntegrationHelper.Teams;
 
-/// <summary>
-/// DataMiner Script Class.
-/// </summary>
 public class Script
 {
-	/// <summary>
-	/// The Script entry point.
-	/// </summary>
-	/// <param name="engine">Link with SLAutomation process.</param>
 	public void Run(Engine engine)
 	{
-		ChatIntegrationHelper chatIntegrationHelper = null;
+		var chatIntegrationHelper = new ChatIntegrationHelperBuilder().Build();
 		try
 		{
 			var ownerEmailParam = engine.GetScriptParam("Team Owner Email");
@@ -90,22 +31,21 @@ public class Script
 				return;
 			}
 
-			var factory = new ChatIntegrationHelperFactory();
-			chatIntegrationHelper = factory.Create(
-				log => engine.Log(log, LogType.Debug, 1),
-				log => engine.Log(log, LogType.Information, 1),
-				log => engine.Log(log, LogType.Error, 1));
+			ITeam team;
 
-			var response = chatIntegrationHelper.Teams.TryCreateTeam(teamNameParam.Value, ownerEmailParam.Value);
-			if (response.Error)
+			try
 			{
-				engine.ExitFail($"Couldn't create the team with error {response.ErrorMessage}.");
+				team = chatIntegrationHelper.Teams.TryCreateTeam(teamNameParam.Value, ownerEmailParam.Value);
+			}
+			catch (TeamsChatIntegrationException e)
+			{
+				engine.ExitFail($"Couldn't create the team with error {e.Message}.");
 				return;
 			}
 
-			teamsMemoryFile.Set($"{teamNameParam.Value} ({response.Team.TeamId})", response.Team.TeamId);
+			teamsMemoryFile.Set($"{teamNameParam.Value} ({team.TeamId})", team.TeamId);
 
-			engine.ExitSuccess($"The team with id {response.Team.TeamId} should be created soon!");
+			engine.ExitSuccess($"The team with id {team.TeamId} should be created soon!");
 		}
 		catch (ScriptAbortException)
 		{
