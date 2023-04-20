974d029f0affcd16d4d4cc7329230287f10a994e
Jordy Ampe <jordy.ampe@skyline.be>
2023-04-20 13:42:54 +0200

    Add input example

--- CustomCommandExamples/Parameter Input Example_1/Parameter Input Example_1.cs
+++ CustomCommandExamples/Parameter Input Example_1/Parameter Input Example_1.cs
@@ -11,6 +11,14 @@ public class Script
 	/// <param name="engine">Link with SLAutomation process.</param>
 	public void Run(Engine engine)
 	{
+		var parameterInput = engine.GetScriptParam("Parameter Input");
+		if (string.IsNullOrWhiteSpace(parameterInput?.Value))
+		{
+			engine.ExitFail("'Parameter Input' parameter is required.");
+			return;
+		}
 
+		// Return the input as an output
+		engine.AddScriptOutput("Parameter Input", parameterInput.Value);
 	}
 }
