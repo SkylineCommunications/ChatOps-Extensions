adc7a06524ef9b4247e0ce07fd72218f87dd623b
Jordy Ampe <jordy.ampe@skyline.be>
2024-02-06 12:54:18 +0100

    Add new examples

--- ChatIntegrationExamples/SendChannelNotificationCardWithButton_1/Properties/AssemblyInfo.cs
+++ ChatIntegrationExamples/SendChannelNotificationCardWithButton_1/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("SendChannelNotificationCard_1")]
+[assembly: AssemblyTitle("SendChannelNotificationCardWithButton_1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Skyline Communications")]
-[assembly: AssemblyProduct("SendChannelNotificationCard_1")]
+[assembly: AssemblyProduct("SendChannelNotificationCardWithButton_1")]
 [assembly: AssemblyCopyright("Copyright © Skyline Communications")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
-[assembly: Guid("6BA787C0-98F6-4535-A4E4-4157582309D3")]
+[assembly: Guid("6D849F1A-4127-4F9D-9085-F1E18FFE36B3")]
 [assembly: AssemblyVersion("1.0.0.0")]
--- ChatIntegrationExamples/SendChatNotificationCardWithButton_1/Properties/AssemblyInfo.cs
+++ ChatIntegrationExamples/SendChatNotificationCardWithButton_1/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("SendChannelNotificationCard_1")]
+[assembly: AssemblyTitle("SendChatNotificationCardWithButton_1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Skyline Communications")]
-[assembly: AssemblyProduct("SendChannelNotificationCard_1")]
+[assembly: AssemblyProduct("SendChatNotificationCardWithButton_1")]
 [assembly: AssemblyCopyright("Copyright © Skyline Communications")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
-[assembly: Guid("6BA787C0-98F6-4535-A4E4-4157582309D3")]
+[assembly: Guid("8EDFB01C-45FB-4B80-B86D-65D3F7DC31E1")]
 [assembly: AssemblyVersion("1.0.0.0")]
